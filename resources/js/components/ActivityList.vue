465eda50934ffc3ddfb97c1cddf7e81cfb31a7a2
GuyTito <profkofi@gmail.com>
2021-07-17 14:33:03 +0000

    display updated and created activities on same page

---
+++ resources/js/components/ActivityList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div v-if="activities.length">
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Activity</th>
+            <th>Status</th>
+            <th>Remark</th>
+            <th>Created By</th>
+            <th>Date Created</th>
+            <th>Date Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(activity, counter) in activities" :key="activity.id">
+            <td>{{ counter + 1 }}</td>
+            <td>
+              <router-link :to="`/activity/show/${activity.id}`">{{
+                activity.activity
+              }}</router-link>
+            </td>
+            <td>{{ activity.status }}</td>
+            <td>{{ activity.remark || "None" }}</td>
+            <td>{{ activity.user.name }}</td>
+            <td>
+              {{ moment(activity.created_at).format("D-MMM-YYYY h:mm:ss a") }}
+            </td>
+            <td>
+              {{ moment(activity.updated_at).format("D-MMM-YYYY h:mm:ss a") }}
+            </td>
+            <td>
+              <router-link :to="`/activity/edit/${activity.id}`"
+                >Update</router-link
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div v-else>No activities available.</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    activities: Array,
+  },
+  computed: {
+    moment: () => moment,
+  }
+};
+</script>
+
+<style>
+</style>
--- resources/js/views/activity/Index.vue
+++ resources/js/views/activity/Index.vue
@@ -3,88 +3,39 @@
     <h2>activities</h2>
     <router-link to="/activity/create">Create Activity</router-link>
 
-    <div>
-      <button @click="createdToday">createdToday</button>
-      <button @click="updatedToday">updatedToday</button>
-    </div>
-    
-    <div v-if="activities.length">
-      <table>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Activity</th>
-            <th>Status</th>
-            <th>Remark</th>
-            <th>Created By</th>
-            <th>Date Created</th>
-            <th>Date Updated</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(activity, counter) in activities" :key="activity.id">
-            <td>{{ counter + 1 }}</td>
-            <td><router-link :to="`/activity/show/${activity.id}`">{{ activity.activity }}</router-link></td>
-            <td>{{ activity.status }}</td>
-            <td>{{ activity.remark || "None" }}</td>
-            <td>{{ activity.user.name }}</td>
-            <td>{{ moment(activity.created_at).format("D-MMM-YYYY h:mm:ss a") }}</td>
-            <td>{{ moment(activity.updated_at).format("D-MMM-YYYY h:mm:ss a") }}</td>
-            <td><router-link :to="`/activity/edit/${activity.id}`">Update</router-link></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div v-else>
-      No activities available.
-    </div>
+    <h3>activities updated_today</h3>
+    <ActivityList :activities="updated_today" />
+
+    <h3>activities created_today</h3>
+    <ActivityList :activities="created_today" />
   </div>
 </template>
 
 <script>
+import ActivityList from "../../components/ActivityList.vue";
+
 export default {
-  data(){
-    return {
-      activities: []
-    }
-  },
+  components: { ActivityList },
   computed: {
     moment: () => moment,
-    // activities(){
-    //   return this.$store.state.activities
-    // },
-  },
-  methods:{
-    createdToday(){
-      let today = this.$store.state.activities.filter(
-        (activity) =>
-          moment(activity.created_at).format("D-MMM-YYYY") >=
-          moment().format("D-MMM-YYYY")
-      );
-      this.activities =  today
+    updated_today(){
+      let today = this.$store.state.activities.filter((activity) =>{
+        if (!activity.activity_updates.length) return false
+        let latest_update = activity.activity_updates.slice(-1)[0]
+        return moment(latest_update.created_at).format("D-MMM-YYYY") >= moment().format("D-MMM-YYYY")
+      });
+      return today
     },
-    updatedToday(){
+    created_today(){
       let today = this.$store.state.activities.filter(
         (activity) =>
-          moment(activity.updated_at).format("D-MMM-YYYY") >=
-          moment().format("D-MMM-YYYY")
+          moment(activity.created_at).format("D-MMM-YYYY") >= moment().format("D-MMM-YYYY")
       );
-      this.activities =  today
+      return today
     },
   },
   mounted() {
-    this.$store.dispatch("fetchAllActivities")
-    axios.get('/api/activity', {
-        headers: {
-          Authorization: `Bearer ${this.$store.state.token}`
-        }
-      })
-      .then(response => {
-        this.activities = response.data
-      })
-      .catch(err => {
-        console.log(err.response.data);
-      })
+    this.$store.dispatch("fetchAllActivities");
   },
 };
 </script>
