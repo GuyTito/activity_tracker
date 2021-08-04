656059f964dad77a68cc2dbcdaa6fd932b50ef0c
GuyTito <profkofi@gmail.com>
2021-08-04 12:36:22 +0000

    search by date addon

--- resources/js/components/SearchActivityByDate.vue
+++ resources/js/components/SearchActivityByDate.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
     <form @submit.prevent="searchActivityByDate">
-      <label for="activity_date">Search activity by custom duration</label>
-      <input
-        type="datetime-local"
-        v-model="activity_date"
-        name="activity_date"
-        id="activity_date"
-      />
-      <button type="submit">Search</button>
+      <div class="form-group">
+        <label for="activity_date" class="col-form-label">Search activity by custom duration</label>
+        <div class="col-md-4">
+          <div class="input-group">
+            <input
+              type="datetime-local"
+              v-model="activity_date"
+              name="activity_date"
+              id="activity_date"
+              class="form-control"
+            />
+            <button type="submit" class="btn btn-outline-secondary">Search</button>
+          </div>
+        </div>
+      </div>
     </form>
   </div>
 </template>
