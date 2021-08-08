a1ba14ef281a42cae2c616e9aa4cee5f0d790ea2
GuyTito <profkofi@gmail.com>
2021-08-08 12:23:21 +0000

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -13,7 +13,7 @@
   <div id="app">
     <app></app>
   </div>
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ secure_asset('js/app.js') }}" defer></script>
 </body>
 
 </html>
