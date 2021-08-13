dbd8ca7b85d1d29d6a285de870ae33610c490305
GuyTito <profkofi@gmail.com>
2021-08-13 19:27:08 +0000

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,10 +9,26 @@
   <title>Activity Tracker</title>
 </head>
 
-<body class="antialiased">
-  <div id="app">
+<body class="d-flex flex-column min-vh-100">
+  <div id="app" class=" wrapper flex-grow-1">
     <app></app>
   </div>
+
+  <footer class="text-center p-2" style="background-color: rgb(240, 239, 239)">
+    <a
+      class="text-decoration-none text-muted mx-3"
+      href="https://twitter.com/QuistKofi"
+      target="blank"
+      >My Twitter</a
+    >
+    <a
+      class="text-decoration-none text-muted mx-4"
+      href="https://github.com/GuyTito/activity_tracker"
+      target="blank"
+      >Source</a
+    >
+  </footer>
+
   <script src="{{ mix('js/app.js') }}" defer></script>
 </body>
 
